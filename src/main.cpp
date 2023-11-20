e674124f9dc7015f5e59d3d8dff2b77dcd2e32ac
LouisePrd <louise.peridy22@gmail.com>
2023-11-20 10:32:57 +0100

    second function

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,29 @@ void greenImage(sil::Image image)
     image.save("output/logo-vert.png");
 }
 
+void swapRedandBlue(sil::Image image)
+{
+    for (glm::vec3 &color : image.pixels())
+    {
+        std::swap(color.r, color.b);
+    }
+    image.save("output/logo-swap.png");
+}
+
+void blackAndWhite(sil::Image image)
+{
+    for (glm::vec3 &color : image.pixels())
+    {
+        float greyLevel = (color.r + color.g + color.b) / 3;
+        //color = greyLevel;
+    }
+    image.save("output/logo-nb.png");
+}
+
 int main()
 {
     sil::Image image{"images/logo.png"};
-    greenImage(image);
+    // greenImage(image);
+    //blackAndWhite(image);
+    swapRedandBlue(image);
 }
